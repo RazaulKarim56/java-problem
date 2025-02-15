f99a2bfa37a5d969ebfff8d2dda5e8a9cda88588
Md Razaul Karim <riyadhvai56@gmail.com>
2025-02-15 13:34:31 +0600

    Add files via upload

---
+++ ArithmeticDemo.java
@@ -0,0 +1,16 @@
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        int n1=20,n2=10,result;
+        result=n1+n2;
+        System.out.println("sum="+result);
+        result=n1-n2;
+        System.out.println("sub="+result);
+        result =n1*n2;
+        System.out.println("multiplication="+result);
+        result=n1/n2;
+        System.out.println("dibition="+result);
+        result=n1%n2;
+        System.out.println("reminder="+result);
+
+    }
+}
